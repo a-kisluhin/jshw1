5c8a9578ca81b7faefef3fa668a0afd3b4a5f508
a-kisluhin <a-kisluhin@mail.ru>
2017-07-20 06:47:27 +0300

    Add files via upload

---
+++ hw1.js
@@ -0,0 +1,12 @@
+var firstParagraph = document.getElementById('one');
+alert (firstParagraph.innerText);
+
+var secondParagraph = document.getElementById('two');
+secondParagraph.innerText = 'Hello world';
+
+var secondParagraph = document.getElementById('ttt');
+secondParagraph.innerText = 'Good buy world';
+
+var span;
+span = document.getElementById('ttt');
+span.innerHTML = 'The End';
